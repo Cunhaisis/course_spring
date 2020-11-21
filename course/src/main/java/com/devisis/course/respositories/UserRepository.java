1ceeb81d4b9acbb3fcaaf8571a30ff9fbddec435
Cunhaisis <isis.cunha13@gmail.com>
2020-11-20 22:13:26 -0300

    Service layer, component registration

--- course/src/main/java/com/devisis/course/respositories/UserRepository.java
+++ course/src/main/java/com/devisis/course/respositories/UserRepository.java
@@ -1,9 +1,12 @@
 package com.devisis.course.respositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devisis.course.entites.User;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long>{
 	
 
---
+++ course/src/main/java/com/devisis/course/service/UserService.java
@@ -0,0 +1,30 @@
+package com.devisis.course.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devisis.course.entites.User;
+import com.devisis.course.respositories.UserRepository;
+
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+	
+
+}
