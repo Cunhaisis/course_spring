4e3f17de0f3a6dc0c5e22cee41452eebbb698338
Cunhaisis <isis.cunha13@gmail.com>
2020-11-21 20:45:33 -0300

    Payment, one-to-one association

--- course/src/main/java/com/devisis/course/config/TestConfig.java
+++ course/src/main/java/com/devisis/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.devisis.course.entites.Category;
 import com.devisis.course.entites.Order;
 import com.devisis.course.entites.OrderItem;
+import com.devisis.course.entites.Payment;
 import com.devisis.course.entites.Product;
 import com.devisis.course.entites.User;
 import com.devisis.course.entites.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T19:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.saveAll(Arrays.asList(o1));
 
 	}
 
